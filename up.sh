b96a58187572a61c60ba77551cf7006d19921f71
Julius Michaelis <gitter@liftm.de>
2020-01-07 13:42:02 +0900

    Cronjob script for polling the upx repo and triggering hub.docker.com through github.com -.-

---
+++ up.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if test "$(git branch --show-current)" != trigger-build; then
+	echo "Not on branch trigger-build, I don't like it." 1>&2
+	exit -1
+fi
+git ls-remote -q https://github.com/upx/upx refs/heads/devel 2>/dev/null | head -n1 | sed 's/\s.*$//' >trigger-build-for
+git add trigger-build-for
+if git diff --name-only HEAD | grep -q trigger-build-for; then
+	git commit -qq -m "Trigger build $(cat trigger-build-for)" trigger-build-for
+fi
+git push -f -u origin trigger-build # always push, just in case this fails occasionally
